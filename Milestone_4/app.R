1504cc8c03f4a607c70489ea517c63915c0d2a0b
Fahad Alkhaja <fhd.alkhaja@gmail.com>
2021-03-12 22:20:20 -0500

    commit 2

--- Milestone_4/app.R
+++ Milestone_4/app.R
@@ -22,10 +22,7 @@ ui <- navbarPage(
                data. It has data about competitions, lineups, events (passes,
                shots, tackles, dribbles) and any in match event data. It is yet
                to be determined whether I will need to pay or find some other 
-               way to  access the full dataset by obtaining an API. I also have
-               to make sure that the data found (if using the free dataset) 
-               corresponds to the tracking data (if in the free Metrica Sports
-               dataset."),
+               way to  access the full dataset by obtaining an API."),
              h3("Data Source 2: Metrica Sports"),
              p("This is the data source I will be using that contains position 
                and tracking data of players. The goal currently in my mind 
@@ -34,20 +31,25 @@ ui <- navbarPage(
                increasingly important as soccers' players movement and starting 
                position with respect and with or without the ball becomes as 
                paramount as the actions they complete with it that will be 
-               recorded in the Statsbomb events dataset. In a similar scenario,
-               I am also usnure whether the free data they have provides enough
-               data and corresponds to the Statsbomb one or whether I will need
-               to obtain an API. Wyscout also has some interesting data that I 
-               could leverage if I'm going to end up getting an API.")),
+               recorded in the Statsbomb events dataset. Wyscout also has some 
+               interesting data that I could leverage if I'm going to end up 
+               getting an API.")),
     tabPanel("About", 
              titlePanel("About"),
              h3("Project Background and Motivations"),
-             p("Hello, this is where I talk about my project."),
+             p("Hello, this is where I talk about my project. I have not completely
+             nailed down what I will be doing for my project. However, I would 
+             like to use event and tracking data to allow an outsider to be able
+             to search the game and visualize what happened. For example, if
+             somebody is interested in goals scored. I would make it such that
+             you can filter for such an event and specify the time before the 
+             goal was scored and visualize the player and ball movements according
+             to the tracking data available."),
              h3("About Me"),
              p("My name is Fahad Alkhaja and I study Mechanical Engineering. 
              You can reach me at falkhaja@college.harvard.edu."),
              h3("Repo URL"),
-             p("https://github.com/Falkhaja/milestone3_finalproject_gov1005.git")))
+             p("https://github.com/Falkhaja/milestone4_finalproj_Gov_1005.git")))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
